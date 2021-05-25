f60b071dfc394bd057bd4a57d0196b52a75d2065
Jelena <jelena.durovic@powerhrg.com>
2021-05-25 10:30:13 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+   const newArray = []
+    for(let i = 0; i < names.length; i++){
+        //inside the loop, do something with each individual element
+     newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    
+    }
+    return newArray
+    //after the loop, return some result of what occured above
+}
+
+
+function countDown(num){
+    while(num >- 1) {
+       console.log(num --)
+    }
+}
+       
+//function countDown(num){
+
+    //for(let i = num; i > num.length; i --){
+       // console.log(num)
+    //}
+//}
+
+
